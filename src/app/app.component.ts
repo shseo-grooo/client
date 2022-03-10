07b7eda459125010929e435d513d0e047e7f0512
shseo-grooo <shseo@grooo.co.jp>
2022-03-10 15:49:34 +0900

    v1.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { MessageService } from './message.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Blue Green 통신 테스트 - v1.7'
+  title = 'Blue Green 통신 테스트 - v1.8'
 
   constructor(
     private apiService: ApiService,
